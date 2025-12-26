ffca22ab6118c5db49f25394318e5351a7ec0176
stosic623it <stosic623it@raf.rs>
2025-12-26 16:22:44 +0100

    izmene github akcija

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <!-- @if (!app()->runningUnitTests()) -->
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <!-- @endif -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     </head>
     <body class="font-sans antialiased">
