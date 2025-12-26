f78cc31d16a8b05d7eeffc91cf7247b4ea8c404c
stosic623it <stosic623it@raf.rs>
2025-12-26 16:14:19 +0100

    izmene

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @if (!app()->runningUnitTests())
+        <!-- @if (!app()->runningUnitTests()) -->
             @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @endif
+        <!-- @endif -->
 
     </head>
     <body class="font-sans antialiased">
