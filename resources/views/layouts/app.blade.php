0222b00fbb698742ed38d5c27d036a973174d37d
stosic623it <stosic623it@raf.rs>
2025-12-26 16:07:52 +0100

    nove izmene testova

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,10 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @if (!app()->runningUnitTests())
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @endif
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
